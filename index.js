a654685d9d143835dd8b0b12613bcc84a4f6f46a
yaoyonstudio <yaoyonstudio@gmail.com>
2018-06-29 10:04:28 +0800

    20180629

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  .set('view engine', 'pug')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
